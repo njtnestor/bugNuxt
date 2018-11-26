a1aa0e5d2e1ecf6b2c881c85f36f771be0e2f3b6
njtnestor <jodar.infmtic@gmail.com>
2018-11-26 21:29:39 +0100

    init

---
+++ pages/_knifes/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <div>
+      false!
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  validate ({ params, query, store}) {
+    return false
+    //return cuchillosCategoria.some((x) => x.subCategorias.some((category) => category.route === params.cuchillos))
+    //return cuchillosCategoria.some((x) => x.subCategorias.some((category) => category.route === params.cuchillos))
+    //return true;
+  }
+}
+</script>
+
+<style>
+
+
+</style>
